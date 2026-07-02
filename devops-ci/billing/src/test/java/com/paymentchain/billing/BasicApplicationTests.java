1c141896362c0a24c5499f9d2bec754ef3fa1472
Gilberto Pérez <perezgarridogilb@gmail.com>
2026-07-02 00:06:37 -0600

    test

--- devops-ci/billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ devops-ci/billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -2,15 +2,20 @@ package com.paymentchain.billing;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.runner.RunWith;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 @ExtendWith(MockitoExtension.class)
+// @RunWith(SpringRunner.class)
+@SpringBootTest
+// @ExtendWith(MockitoExtension.class)
 public class BasicApplicationTests  {
 
     @Test
     void contextLoads() {
-       String message = "Default Message Cambio 11";
+       String message = "Default Message Cambio Migrar de Laravel a Spring";
         assertNotNull(message);
     }
 }
